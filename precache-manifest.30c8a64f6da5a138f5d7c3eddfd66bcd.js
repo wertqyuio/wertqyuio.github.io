04b26071ac07f88439ba5bc0465d1c768da872b0
wertqyuio <ultimatenarutofan@gmail.com>
2020-02-21 10:09:38 -0800

    latest website

---
+++ precache-manifest.30c8a64f6da5a138f5d7c3eddfd66bcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad91e001185bd5d2e14013b85fed8e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ce3458afefe8b31ee08",
+    "url": "/static/css/main.61c53a3c.chunk.css"
+  },
+  {
+    "revision": "c2782b2309f94d82922b",
+    "url": "/static/js/2.cda2454f.chunk.js"
+  },
+  {
+    "revision": "7ce3458afefe8b31ee08",
+    "url": "/static/js/main.c97e7575.chunk.js"
+  },
+  {
+    "revision": "f9c055dd03423cdeba44",
+    "url": "/static/js/runtime~main.c58b36c0.js"
+  },
+  {
+    "revision": "6080a2cf122d83ff59debbd83f795211",
+    "url": "/static/media/Piefaceshowdown.6080a2cf.jpg"
+  }
+]);
